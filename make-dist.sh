9bd88cbdfed6bcee3c4243191b12be14e68a00fd
Tobias Oetiker <tobi@oetiker.ch>
2007-12-17 18:35:30 +0000

    included build script

---
+++ make-dist.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+V=1.0.0
+set -x 
+root=du2rrd-$V
+mv qooxdoo/build qooxdoo/build.offline
+for d in htdocs bin man/man1 doc; do
+        mkdir -p $root/$d
+done
+ln -s ../$root/htdocs qooxdoo/build
+(cd qooxdoo; make build)
+rm qooxdoo/build
+mv qooxdoo/build.offline qooxdoo/build
+cp du2rrd $root/bin
+cp example-crontab $root/doc
+cd $root
+pod2man --release=${V} --center=du2rrd bin/du2rrd > man/man1/du2rrd.1
+GROFF_NO_SGR=1 /usr/bin/nroff -man -Tlp man/man1/du2rrd.1 > doc/du2rrd.txt
+svn export svn://oss.oetiker.ch/optools/du2rrd/trunk/qooxdoo src
+cd ..
+tar zcvf $root.tar.gz $root
+rm -rf $root
